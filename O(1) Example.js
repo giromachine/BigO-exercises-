0268b6dd6f22481a8db6dcb6344cd7a51a449564
Carlos Navarro <elgyro99@gmail.com>
2020-09-08 19:52:26 +0000

    -Added examples for O(n). -Added examples for O(1).

---
+++ O(1) Example.js
@@ -0,0 +1,7 @@
+const boxes = [0,1,2,3,4,5,6];
+
+function logFirstBox(boxes){
+  console.log(boxes[0]);
+}
+
+logFirstBox(boxes) // O(1) --> constant time
---
+++ O(n) Example.js
@@ -0,0 +1,11 @@
+const large = new Array(1000).fill('nemo');
+
+function findNemo(array){
+  for(let i = 0; i < array.length; i++){
+    if(array[i] == 'nemo'){
+      console.log('Found Nemo!');
+    }
+  }
+}
+
+findNemo(large); // O(n) --> Linear Time
